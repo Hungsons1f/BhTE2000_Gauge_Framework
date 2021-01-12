b37511c86c004ac1f6e14200f33e5b22c4a030e3
Cydeus <Hungsons1fphu@gmail.com>
2021-01-12 11:31:05 +0700

    First Commit

--- Bar_Flat/Bar_FlatControl/Bar_FlatControl.ts
+++ Bar_Flat/Bar_FlatControl/Bar_FlatControl.ts
@@ -23,12 +23,35 @@ module TcHmi {
                  * @param {TcHmi.Controls.ControlAttributeList} attrs Attributes defined in HTML in a special format (internal, do not use)
                  * @returns {void}
                  */
+
+                protected __uTitle : any;
+                protected __uShowTitle : any;
+                protected __uShowPercent : any;
+                protected __uData : any;
+                protected __uWarningValue : any;
+                protected __uAlarmValue : any;
+                protected __uFillColorNormal : any;
+                protected __uFillColorWarning : any;
+                protected __uFillColorAlarm : any;
+
+                protected __uColorNormal : any;
+                protected __uColorWarning : any;
+                protected __uColorAlarm : any;
+
                 constructor(element: JQuery, pcElement: JQuery, attrs: TcHmi.Controls.ControlAttributeList) {
                     /** Call base class constructor */
                     super(element, pcElement, attrs);
+
                 }
 
                 protected __elementTemplateRoot!: JQuery;
+
+                protected __uGauge: JQuery;
+                protected __uGaugeBody: JQuery;
+                protected __uGaugeFill: JQuery;
+                protected __uGaugeCover: JQuery;
+                protected __uGaugeTitle: JQuery;
+                protected __uGaugePercent: JQuery;
 
 				/**
                   * If raised, the control object exists in control cache and constructor of each inheritation level was called.
@@ -40,6 +63,14 @@ module TcHmi {
                     if (this.__elementTemplateRoot.length === 0) {
                         throw new Error('Invalid Template.html');
                     }
+
+                    this.__uGauge = this.__element.find('.cydeus-bar-gauge');
+                    this.__uGaugeBody = this.__element.find('.cydeus-bar-gauge-body');
+                    this.__uGaugeFill = this.__element.find('.cydeus-bar-gauge-fill');
+                    this.__uGaugeCover = this.__element.find('.cydeus-bar-gauge-cover');
+                    this.__uGaugeTitle = this.__element.find('.cydeus-bar-gauge-title');
+                    this.__uGaugePercent = this.__element.find('.cydeus-bar-gauge-percent');
+
                     // Call __previnit of base class
                     super.__previnit();
                 }
@@ -94,6 +125,381 @@ module TcHmi {
                     * Free resources like child controls etc.
                     */
                 }
+
+                public setTitle(valueNew: any) {
+                    // convert the new value
+                    var convertedValue = TcHmi.ValueConverter.toString(valueNew);
+
+                    // if converted value is null, get internal default
+                    if (convertedValue === null) {
+                        convertedValue = this.getAttributeDefaultValueInternal('Title');
+                    }
+
+                    // skip processing when the value has not changed
+                    if (convertedValue === this.__uTitle) {
+                        return;
+                    }
+
+                    // save new value
+                    this.__uTitle = convertedValue;
+
+                    // Inform the system that the function has a changed result.
+                    TcHmi.EventProvider.raise(this.__id + ".onFunctionResultChanged", ["getTitle"]);
+
+                    // process the new value
+                    this.__processTitle();
+                };
+
+                public getTitle() {
+                    // return the current text
+                    return this.__uTitle;
+                };
+
+                public __processTitle() {
+                    this.__uGaugeTitle.html(this.__uTitle);
+                };
+
+                public setShowTitle(valueNew: any) {
+                    // convert the new value
+                    var convertedValue = TcHmi.ValueConverter.toBoolean(valueNew);
+
+                    // if converted value is null, get internal default
+                    if (convertedValue === null) {
+                        convertedValue = this.getAttributeDefaultValueInternal('ShowTitle');
+                    }
+
+                    // skip processing when the value has not changed
+                    if (convertedValue === this.__uShowTitle) {
+                        return;
+                    }
+
+                    // save new value
+                    this.__uShowTitle = convertedValue;
+
+                    // Inform the system that the function has a changed result.
+                    TcHmi.EventProvider.raise(this.__id + ".onFunctionResultChanged", ["getShowTitle"]);
+
+                    // process the new value
+                    this.__processShowTitle();
+                };
+
+                public getShowTitle  () {
+                    // return the current text
+                    return this.__uShowTitle;
+                };
+
+                public __processShowTitle  () {
+                    if (this.__uShowTitle) {
+                        // show the gauge
+                        this.__uGaugeTitle.css({
+                            visibility: 'visible'
+                        });
+                    } else {
+                        // hide the gauge
+                        this.__uGaugeTitle.css({
+                            visibility: 'collapse'
+                        });
+                    }
+                };
+
+                public setShowPercent  (valueNew: any) {
+                    // convert the new value
+                    var convertedValue = TcHmi.ValueConverter.toBoolean(valueNew);
+
+                    // if converted value is null, get internal default
+                    if (convertedValue === null) {
+                        convertedValue = this.getAttributeDefaultValueInternal('ShowPercent');
+                    }
+
+                    // skip processing when the value has not changed
+                    if (convertedValue === this.__uShowPercent) {
+                        return;
+                    }
+
+                    // save new value
+                    this.__uShowPercent = convertedValue;
+
+                    // Inform the system that the function has a changed result.
+                    TcHmi.EventProvider.raise(this.__id + ".onFunctionResultChanged", ["getShowPercent"]);
+
+                    // process the new value
+                    this.__processShowPercent();
+                };
+
+                public getShowPercent  () {
+                    // return the current text
+                    return this.__uShowPercent;
+                };
+
+                public __processShowPercent  () {
+                    if (this.__uShowPercent) {
+                        // show the gauge
+                        this.__uGaugePercent.css({
+                            visibility: 'visible'
+                        });
+                    } else {
+                        // hide the gauge
+                        this.__uGaugePercent.css({
+                            visibility: 'collapse'
+                        });
+                    }
+                };
+
+                public setData  (valueNew: any) {
+                    // convert the new value
+                    var convertedValue = TcHmi.ValueConverter.toNumber(valueNew);
+
+                    // if converted value is null, get internal default
+                    if (convertedValue === null) {
+                        convertedValue = this.getAttributeDefaultValueInternal('Data');
+                    }
+
+                    // skip processing when the value has not changed
+                    if (convertedValue === this.__uData) {
+                        return;
+                    }
+
+                    // save new value
+                    this.__uData = convertedValue;
+
+                    // Inform the system that the function has a changed result.
+                    TcHmi.EventProvider.raise(this.__id + ".onFunctionResultChanged", ["getData"]);
+
+                    // process the new value
+                    this.__processData();
+                };
+
+                public getData  () {
+                    // return the current text
+                    return this.__uData;
+                };
+
+                public __processData  () {
+                    // set text to html element
+                    //if (this.__elementButton) {
+                    //    this.__elementButton.text(this.__buttonText);
+                    //}
+                    if (!this.__uColorAlarm) {
+                        this.__uColorAlarm = '#ff4646';
+                    }
+                    if (!this.__uColorNormal) {
+                        this.__uColorNormal = '#00ffff';
+                    }
+                    if (!this.__uColorWarning) {
+                        this.__uColorWarning = '#f1f218';
+                    }
+
+                    var value = this.__uData;
+                    if ((value < 0) || (value > 100)) {
+                        return;
+                    }
+                    else if ((value >= 0) && (value < this.__uWarningValue)) {
+                        this.__uGaugeFill.css({
+                            background: this.__uColorNormal//'#00ffff'
+                        });
+                    }
+                    else if ((value >= this.__uWarningValue) && (value < this.__uAlarmValue)) {
+                        this.__uGaugeFill.css({
+                            background: this.__uColorWarning//'#f1f218'
+                        });
+                    }
+                    else {
+                        this.__uGaugeFill.css({
+                            background: this.__uColorAlarm//'#ff4646'
+                        });
+                    }
+
+                    this.__uGaugeFill.css({
+                        transform: 'translateX(' + value + '%)'
+                    });
+                    this.__uGaugePercent.text(value + '%');
+                };
+
+                public setWarningValue  (valueNew: any) {
+                    // convert the new value
+                    var convertedValue = TcHmi.ValueConverter.toNumber(valueNew);
+
+                    // if converted value is null, get internal default
+                    if (convertedValue === null) {
+                        convertedValue = this.getAttributeDefaultValueInternal('WarningValue');
+                    }
+
+                    // skip processing when the value has not changed
+                    if (convertedValue === this.__uWarningValue) {
+                        return;
+                    }
+
+                    // save new value
+                    this.__uWarningValue = convertedValue;
+
+                    // Inform the system that the function has a changed result.
+                    TcHmi.EventProvider.raise(this.__id + ".onFunctionResultChanged", ["getWarningValue"]);
+
+                    // process the new value
+                    this.__processWarningValue();
+                };
+
+                public getWarningValue  () {
+                    // return the current text
+                    return this.__uWarningValue;
+                };
+
+                public __processWarningValue  () {
+                    // set text to html element
+                    //if (this.__elementButton) {
+                    //    this.__elementButton.text(this.__buttonText);
+                    //}
+                    if ((this.__uWarningValue < 0)) {
+                        this.__uWarningValue = 0;
+                    }
+                    else if ((this.__uWarningValue > this.__uAlarmValue)) {
+                        this.__uWarningValue = this.__uAlarmValue;
+                    }
+                };
+
+                public setAlarmValue  (valueNew: any) {
+                    // convert the new value
+                    var convertedValue = TcHmi.ValueConverter.toNumber(valueNew);
+
+                    // if converted value is null, get internal default
+                    if (convertedValue === null) {
+                        convertedValue = this.getAttributeDefaultValueInternal('AlarmValue');
+                    }
+
+                    // skip processing when the value has not changed
+                    if (convertedValue === this.__uAlarmValue) {
+                        return;
+                    }
+
+                    // save new value
+                    this.__uAlarmValue = convertedValue;
+
+                    // Inform the system that the function has a changed result.
+                    TcHmi.EventProvider.raise(this.__id + ".onFunctionResultChanged", ["getAlarmValue"]);
+
+                    // process the new value
+                    this.__processAlarmValue();
+                };
+
+                public getAlarmValue  () {
+                    // return the current text
+                    return this.__uAlarmValue;
+                };
+
+                public __processAlarmValue  () {
+                    // set text to html element
+                    //if (this.__elementButton) {
+                    //    this.__elementButton.text(this.__buttonText);
+                    //}               
+                    if ((this.__uAlarmValue < this.__uWarningValue)) {
+                        this.__uAlarmValue = this.__uWarningValue;
+                    }
+                    else if ((this.__uAlarmValue > 100)) {
+                        this.__uAlarmValue = 100;
+                    }
+                };
+
+                public setFillColorNormal  (valueNew: any) {
+                    // convert the new value
+                    var convertedValue = valueNew;
+
+                    // if converted value is null, get internal default
+                    if (convertedValue === null) {
+                        convertedValue = this.getAttributeDefaultValueInternal('FillColorNormal');
+                    }
+
+                    // skip processing when the value has not changed
+                    if (convertedValue === this.__uFillColorNormal) {
+                        return;
+                    }
+
+                    // save new value
+                    this.__uFillColorNormal = convertedValue;
+
+                    // Inform the system that the function has a changed result.
+                    TcHmi.EventProvider.raise(this.__id + ".onFunctionResultChanged", ["getFillColorNormal"]);
+
+                    // process the new value
+                    this.__processFillColorNormal();
+                };
+
+                public getFillColorNormal  () {
+                    // return the current text
+                    return this.__uFillColorNormal;
+                };
+
+                public __processFillColorNormal  () {
+                    // set text to html element
+                    this.__uColorNormal = this.__uFillColorNormal.color;
+                };
+
+                public setFillColorWarning  (valueNew: any) {
+                    // convert the new value
+                    var convertedValue = valueNew;
+
+                    // if converted value is null, get internal default
+                    if (convertedValue === null) {
+                        convertedValue = this.getAttributeDefaultValueInternal('FillColorWarning');
+                    }
+
+                    // skip processing when the value has not changed
+                    if (convertedValue === this.__uFillColorWarning) {
+                        return;
+                    }
+
+                    // save new value
+                    this.__uFillColorWarning = convertedValue;
+
+                    // Inform the system that the function has a changed result.
+                    TcHmi.EventProvider.raise(this.__id + ".onFunctionResultChanged", ["getFillColorWarning"]);
+
+                    // process the new value
+                    this.__processFillColorWarning();
+                };
+
+                public getFillColorWarning  () {
+                    // return the current text
+                    return this.__uFillColorWarning;
+                };
+
+                public __processFillColorWarning  () {
+                    // set text to html element
+                    this.__uColorWarning = this.__uFillColorWarning.color;
+                };
+
+                public setFillColorAlarm  (valueNew: any) {
+                    // convert the new value
+                    var convertedValue = valueNew;
+
+                    // if converted value is null, get internal default
+                    if (convertedValue === null) {
+                        convertedValue = this.getAttributeDefaultValueInternal('FillColorAlarm');
+                    }
+
+                    // skip processing when the value has not changed
+                    if (convertedValue === this.__uFillColorAlarm) {
+                        return;
+                    }
+
+                    // save new value
+                    this.__uFillColorAlarm = convertedValue;
+
+                    // Inform the system that the function has a changed result.
+                    TcHmi.EventProvider.raise(this.__id + ".onFunctionResultChanged", ["getFillColorAlarm"]);
+
+                    // process the new value
+                    this.__processFillColorAlarm();
+                };
+
+                public getFillColorAlarm  () {
+                    // return the current text
+                    return this.__uFillColorAlarm;
+                };
+
+                public __processFillColorAlarm  () {
+                    // set text to html element
+                    this.__uColorAlarm = this.__uFillColorAlarm.color;
+                };
             }
         }
 
